60e6910f09080d186c7159126aa28942831d2543
aWiedebusch <awiedebusch@smu.edu>
2016-11-08 17:55:20 -0600

    Updated Sprint 3 Form

---
+++ Angular/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app/app.html',
+  styleUrls: [ './app/app.css' ],
+})
+
+export class AppComponent { 
+
+	constructor(){}
+}
