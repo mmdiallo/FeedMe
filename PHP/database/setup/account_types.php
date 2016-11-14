ba2c6db7464817c0bbd3c5b988ba0daf352e99d6
Brittni Watkins <blwatkins@smu.edu>
2016-11-14 14:14:58 -0600

    Complete AccountTypes table creation and population.

--- PHP/database/setup/account_types.php
+++ PHP/database/setup/account_types.php
@@ -4,20 +4,23 @@
     //if (empty($_SERVER['HTTPS'])) {
         //exit;
     //} else {
+
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $conn = new SQLite3($db_filename);
-            
+
             $create_table = 'CREATE TABLE IF NOT EXISTS AccountTypes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
                 type TEXT UNIQUE NOT NULL)';
 
             if ($conn->query($create_table)) {
                 echo 'Table creation sucessful!' . '<br>';
-                $create_type_user = 'INSERT INTO AccountTypes(type) VALUES(\'user\')';
-                $create_type_restaurant = 'INSERT INTO AccountTypes(type) VALUES(\'restaurant\')';
+                
+                $create_type_user = 'INSERT OR IGNORE INTO AccountTypes(type) VALUES(\'user\')';
+                $create_type_restaurant = 'INSERT OR IGNORE INTO AccountTypes(type) VALUES(\'restaurant\')';
+
                 if ($conn->query($create_type_user) && $conn->query($create_type_restaurant)) {
                     echo 'Type creation successful!';
-                }
+                } 
             }
 
             $conn->close();
