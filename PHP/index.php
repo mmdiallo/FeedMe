aea4078667c8676ce65a5730efd852a207e7ec32
Brittni Watkins <blwatkins@smu.edu>
2016-11-14 14:43:22 -0600

    Begin user authentication.

---
+++ PHP/index.php
@@ -0,0 +1,19 @@
+<?php
+    session_set_cookie_params(0, dirname(__FILE__), "", true, true); 
+    session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Feed Me</title>
+    </head>
+    <body>
+        <?php
+            if (empty($_SESSION['authenticated']) || $_SESSION['authenticated'] == false) {
+                echo '<a href="account/login_user.php">Login</a>';
+            } else {
+                echo 'You are logged in.';
+            }
+        ?>
+    </body>
+</html>
