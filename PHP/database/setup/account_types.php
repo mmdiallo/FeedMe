64d048f758cf29d2f2b12bf4e4d4df01ccf8cfed
Brittni Watkins <blwatkins@smu.edu>
2016-11-14 10:53:48 -0600

    Complete account_types table creation.

--- PHP/database/setup/account_types.php
+++ PHP/database/setup/account_types.php
@@ -1,14 +1,27 @@
 <?php
     require 'database_config.php';
 
-    if (empty($_SERVER['HTTPS'])) {
-        exit;
-    } else {
+    //if (empty($_SERVER['HTTPS'])) {
+        //exit;
+    //} else {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            echo 'GET';
-            echo $db_filename;
+            $conn = new SQLite3($db_filename);
+            
+            $create_table = 'CREATE TABLE IF NOT EXISTS AccountTypes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                type TEXT UNIQUE NOT NULL)';
+
+            if ($conn->query($create_table)) {
+                echo 'Table creation sucessful!' . '<br>';
+                $create_type_user = 'INSERT INTO AccountTypes(type) VALUES(\'user\')';
+                $create_type_restaurant = 'INSERT INTO AccountTypes(type) VALUES(\'restaurant\')';
+                if ($conn->query($create_type_user) && $conn->query($create_type_restaurant)) {
+                    echo 'Type creation successful!';
+                }
+            }
+
+            $conn->close();
         }
-        echo 'HTTPS';
         
-    }
+    //}
 ?>
