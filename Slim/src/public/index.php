f62645379d8b207c9850055b894c519433a9daf4
Brittni Watkins <blwatkins@smu.edu>
2016-11-17 12:19:01 -0600

    Clean up code. Add comments to separate index.php file. Rename functions directory to classes. Rename files in classes directory to reflect class name.

--- Slim/src/public/index.php
+++ Slim/src/public/index.php
@@ -1,9 +1,17 @@
 <?php
+// START SESSION
+session_set_cookie_params(0, '/', '', true, true);
+session_name('by_PHPSESSID');
+session_start();
+session_regenerate_id(true);
+
+// SET UP SLIM APPLICATION
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require '../functions/database_creation.php';
+require '../classes/DatabaseCreation.php';
+require '../classes/AuthenticationHandler.php';
 
 $config['displayErrorDetails'] = true;
 $config['db']['filename'] = '../database/feed_me.sqlite';
@@ -18,7 +26,10 @@ $container['db'] = function($c) {
     return $sqlite;
 };
 
-$https_middleware = function(Request $request, Response $response, $next) {
+// MIDDLEWARE
+
+// if request not https, redirect
+$https_mw = function(Request $request, Response $response, $next) {
     if (empty($_SERVER['HTTPS'])) {
         header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
         exit;
@@ -28,38 +39,88 @@ $https_middleware = function(Request $request, Response $response, $next) {
     return $response;
 };
 
-$mw_1 = function(Request $request, Response $response, $next) {
-    $response->getBody()->write("1, before ");
+$app->add($https_mw);
+
+// check session authentication, send authentication and session to route
+$session_mw = function(Request $request, Response $response, $next) {
+    $authentication = new AuthenticationHandler;
+    $current_auth = $authentication->checkAuthentication();
+    $request->withAttribute('session', $_SESSION);
+    $request->withAttribute('auth', $current_auth);
     $response = $next($request, $response);
-    $response->getBody()->write("1, after");
     return $response;
 };
 
-$app->group('/', function() use ($app){
-    $app->get('', function(Request $request, Response $response) {
-        $response->getBody()->write('<h1>Home</h1>');
-        $response->getBody()->write('<a href="/database_setup/0"> Database Setup </a>');
-        return $response;
-    });
+// if not authenticated, redirect to home
+$authentication_mw = function(Request $request, Response $response, $next) {
+    $response = $next($request, $response);
+    return $response;
+};
 
-    $app->get('database_setup/{num: [\d]+}', function(Request $request, Response $response, $args) {
-        $script_number = (int)$args['num'];
-        $database_creation = new DatabaseCreation($this->db);
-        $result = $database_creation->createTable($script_number);
-        $response->getBody()->write($result);
+// if authenticated, redirect to home
+// prevents access to login and create account pages for already authenticated users
+$authentication_mw_2 = function(Request $request, Response $response, $next) {
+    $response = $next($request, $response);
+    return $response;
+};
 
-        if ($result != 'invalid request') {
-            $next_script_number = $script_number + 1;
-            $next_page = '/database_setup/' . $next_script_number;
-            $next_page_link = '<br><a href="' . $next_page . '"> Next </a>';
-            $response->getBody()->write($next_page_link);
-        }
+// ROUTES
 
-        $response->getBody()->write('<br><br><a href="/"> Home </a>');
+// Home Page
+$app->get('/', function(Request $request, Response $response) {
+    $response->getBody()->write('<h1>Home</h1>');
+    $auth = $request->getAttribute('auth');
 
-        return $response;
-    });
+    if (!$auth) {
+        $begin_div = '<div style="margin: 8;">';
+        $login_link = '<a style="padding: 8;" href="/login"> Login </a>';
+        $create_user_account_link = '<a style="padding: 8;" href="/create_user_account"> Create User Account </a>';
+        $create_restaurant_account_link = '<a style="padding: 8;" href="/create_restaurant_account"> Create Restaurant Account </a>';
+        $end_div = '</div>';
+        $response->getBody()->write($begin_div);
+        $response->getBody()->write($login_link);
+        $response->getBody()->write($create_user_account_link);
+        $response->getBody()->write($create_restaurant_account_link);
+        $response->getBody()->write($end_div);
+    }
 
-})->add($https_middleware);
+    $response->getBody()->write('<div style="margin: 8;"><a style="padding: 8;" href="/database_setup/0"> Database Setup </a></div>');
+    return $response;
+})->add($session_mw);
 
+// Login Page
+$app->get('/login', function(Request $request, Response $response) {
+    return $response;
+});
+
+// Create User Account Page
+$app->get('/create_user_account', function(Request $request, Response $response) {
+    return $response;
+});
+
+// Create Restaurant Account Page
+$app->get('/create_restaurant_account', function(Request $request, Response $response) {
+    return $response;
+});
+
+
+// Database Creation Pages
+$app->get('/database_setup/{num: [\d]+}', function(Request $request, Response $response, $args) {
+    $script_number = (int)$args['num'];
+    $database_creation = new DatabaseCreation($this->db);
+    $result = $database_creation->createTable($script_number);
+    $response->getBody()->write($result);
+
+    if ($result != 'invalid request') {
+        $next_script_number = $script_number + 1;
+        $next_page = '/database_setup/' . $next_script_number;
+        $next_page_link = '<br><a href="' . $next_page . '"> Next </a>';
+        $response->getBody()->write($next_page_link);
+    }
+
+    $response->getBody()->write('<br><br><a href="/"> Home </a>');
+    return $response;
+});
+
+// RUN THE APPLICATION
 $app->run();
