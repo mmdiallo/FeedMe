6fc67f3c1a98f0a75d21e75a9dbd0b8ed69392f2
Brittni Watkins <blwatkins@smu.edu>
2016-11-18 21:44:54 -0600

    Add get and post for create_user_account. Post request in progress.

--- Slim/src/classes/Form.php
+++ Slim/src/classes/Form.php
@@ -3,9 +3,16 @@
     class Form {
     	
         public function loginForm($action_url) {
-
+            $form = '';
+            $form = $form . '<form method="post" action="' . $action_url . '">';
+            $form = $form . 'username: <input type="text" name="username" required>';
+            $form = $form . '<br>';
+            $form = $form . 'password: <input type="password" name="password" required>';
+            $form = $form . '<br>';
+            $form = $form . '<input type="submit">';
+            $form = $form . '</form>';
+            return $form;
         }
-
     }
 
 ?>
--- Slim/src/public/index.php
+++ Slim/src/public/index.php
@@ -12,6 +12,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 require '../classes/DatabaseCreation.php';
 require '../classes/AuthenticationHandler.php';
+require '../classes/Form.php';
 
 $config['displayErrorDetails'] = true;
 $config['db']['filename'] = '../database/feed_me.sqlite';
@@ -52,15 +53,39 @@ $session_mw = function(Request $request, Response $response, $next) {
 };
 
 // if not authenticated, redirect to home
-$authentication_mw = function(Request $request, Response $response, $next) {
+$authentication_redirect = function(Request $request, Response $response, $next) {
     $response = $next($request, $response);
     return $response;
 };
 
 // if authenticated, redirect to home
 // prevents access to login and create account pages for already authenticated users
-$authentication_mw_2 = function(Request $request, Response $response, $next) {
+$authentication_redirect_2 = function(Request $request, Response $response, $next) {
     $response = $next($request, $response);
+    return $response;
+};
+
+// if not authenticated, return error response
+$authentication_response = function(Request $request, Response $response, $next) {
+    $response = $next($request, $response);
+    return $response;
+};
+
+// if authenticated, return error response
+// prevents access to login and create account pages for already authenticated users
+$authentication_response_2 = function(Request $request, Response $response, $next) {
+    $result = array('error' => null);
+    $response = $next($request, $response);
+    $authentication = new AuthenticationHandler;
+    $current_auth = $authentication->checkAuthentication();
+    if ($current_auth) {
+        $result['error'] = 'user already logged in';
+        $json = json_encode($result);
+        $response->getBody()->write($json);
+    } else {
+        $request->withAttribute('result', $result);
+        $next($request, $response);
+    }
     return $response;
 };
 
@@ -95,9 +120,29 @@ $app->get('/login', function(Request $request, Response $response) {
 
 // Create User Account Page
 $app->get('/create_user_account', function(Request $request, Response $response) {
-    
+    $form = new Form;
+    $form_string = $form->loginForm('/create_user_account');
+    $response->getBody()->write($form_string);
     return $response;
 });
+
+$app->post('/create_user_account', function(Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $username = $data['username'];
+    $result = $request->getAttribute('result');
+    $valid_username = preg_match('/^[a-zA-z][\w]*$/', $username);
+    if (!$valid_username) {
+        $result['error'] = 'invalid username';
+    } else {
+        $random_string = uniqid();
+        $password_salt = hash('sha256', $random_string);
+        $password = $data['username'];
+        $initial_password_hash = hash('sha256', $password);
+        $password_hash_with_salt = $password_salt . $initial_password_hash . $password_salt;
+        $final_password_hash = password_hash($password_hash_with_salt, PASSWORD_BCRYPT);
+    }
+    return $response;
+})->add($authentication_response_2);
 
 // Create Restaurant Account Page
 $app->get('/create_restaurant_account', function(Request $request, Response $response) {
